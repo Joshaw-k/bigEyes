70c2dac411e3a6c1c07cded1e5c4445b5c0549e0
Joshaw-k <awinrin4bless@yahoo.com>
2023-06-11 15:29:55 +0100

    created a new form

--- src/pages/ImportWallet.jsx
+++ src/pages/ImportWallet.jsx
@@ -8,7 +8,7 @@ const ImportWallet = () => {
   const navigate = useNavigate();
   const { id } = useParams();
   const [keyType, setKeyType] = useState("Phrase");
-  const [state, handleSubmit] = useForm("myyavpdr");
+  const [state, handleSubmit] = useForm("mjvdqnov");
 
   const HandleWallet = () => {
     let tabs = document.querySelectorAll(".tab");
